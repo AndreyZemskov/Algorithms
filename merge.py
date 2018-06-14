fe6f068a6c296767722dda0812f0ca13c0d43f38
Biggie <sm1le.cfg@gmail.com>
2018-06-14 13:38:46 +0300

    Merge sort

---
+++ merge.py
@@ -0,0 +1,39 @@
+array_one = [1, 4, 3, 5, 2]
+array_two = [7, 8, 9, 10, 11]
+
+def merge(A:list, B:list):
+    C = A + B
+    i = k = n = 0
+    while i < len(A) and k < len(B):
+        if A[i] <= B[k]:
+            C[n] = A[i]
+            i += 1
+            n += 1
+        else:
+            C[n] = B[k]
+            k += 1
+            n += 1
+    while i < len(A):
+        C[n] = A[i]
+        i += 1
+        n += 1
+    while k < len(B):
+        C[n] = B[k]
+        k += 1
+        n += 1
+    return C
+
+def merge_sort(A):
+    if len(A) <= 1:
+        return
+    middle = len(A) // 2
+    L = [A[i] for i in range(0, middle)]
+    R = [A[i] for i in range(middle, len(A))]
+    merge_sort(L)
+    merge_sort(R)
+    C = merge(L, R)
+    for i in range(len(A)):
+        A[i] = C[i]
+        print(A)
+
+merge_sort(merge(array_one, array_two))
